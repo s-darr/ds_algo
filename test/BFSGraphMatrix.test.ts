13e6b4ec898e8651b3818c0e72489fc140f75aa4
s-darr <darrsuleman@gmail.com>
2023-08-04 17:01:59 +0100

    BFS on adjacency matrix

---
+++ test/BFSGraphMatrix.test.ts
@@ -0,0 +1,34 @@
+import bfs from "../src/BFSGraphMatrix"; // replace with the correct import path
+type WeightedAdjacencyMatrix = number[][];
+describe("bfs", () => {
+  it("returns the shortest path from source to target", () => {
+    const graph: WeightedAdjacencyMatrix = [
+      [0, 1, 0, 0],
+      [1, 0, 1, 0],
+      [0, 1, 0, 1],
+      [0, 0, 1, 0],
+    ];
+    const source = 0;
+    const target = 3;
+    const expectedPath = [0, 1, 2, 3];
+
+    const result = bfs(graph, source, target);
+
+    expect(result).toEqual(expectedPath);
+  });
+
+  it("returns null if there is no path from source to target", () => {
+    const graph: WeightedAdjacencyMatrix = [
+      [0, 1, 0, 0],
+      [1, 0, 0, 0],
+      [0, 0, 0, 1],
+      [0, 0, 1, 0],
+    ];
+    const source = 0;
+    const target = 3;
+
+    const result = bfs(graph, source, target);
+
+    expect(result).toBeNull();
+  });
+});
